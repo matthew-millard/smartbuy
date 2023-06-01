ee4dcb8644ba74f3c5f531a90f36b5393622535a
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2023-06-01 13:07:40 -0400

    Create and seed database with test data

--- Develop/server.js
+++ Develop/server.js
@@ -1,26 +1,26 @@
-const express = require('express')
-const routes = require('./routes')
+const express = require('express');
+const routes = require('./routes');
 // import sequelize connection
-const sequelize = require('./config/connection')
+const sequelize = require('./config/connection');
 
-const app = express()
-const PORT = process.env.PORT || 3001
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(routes)
+app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
 const startServer = async () => {
-	try {
-		await sequelize.sync()
-		app.listen(PORT, () => {
-			console.log(`App listening on port ${PORT}!`)
-		})
-	} catch (err) {
-		console.error(err)
-	}
-}
+  try {
+    await sequelize.sync({ force: true });
+    app.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}!`);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
 
-startServer()
+startServer();
